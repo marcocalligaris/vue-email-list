542280bdd9673af6cff6918a20cbeb935a672921
marcocalligaris <marcocally@gmail.com>
2022-07-21 10:58:08 +0200

    List of addresses done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const app = new Vue({
     el: '#root',
     data: {
-        items: 10,
         // address: '',
         addresses: [],
     },
@@ -12,14 +11,12 @@ const app = new Vue({
     //     }),
     // },
 
-    methods: {
-        getEmailAddresses() {
-            for (let i = 1; i <= items; i++) {
+    created() {
+            for (let i = 1; i <= 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-                    const address = res.data.response
+                    const address = res.data.response;
                     this.addresses.push(address);
                 })
             }
-        }
     },
 })
